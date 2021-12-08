2f1a85e9964d81f9fcc271d18267ab1467cc5090
DH-Cang <cdh573885@outlook.com>
2021-12-08 23:34:14 +0800

    fix the bug of no window's title

--- base/test.cpp
+++ base/test.cpp
@@ -30,6 +30,8 @@ void test::handleInput() {
 }
 
 void test::renderFrame() {
+	showFpsInWindowTitle();
+
 	glClearColor(_clearColor.r, _clearColor.g, _clearColor.b, _clearColor.a);
 	glClear(GL_COLOR_BUFFER_BIT);
 
