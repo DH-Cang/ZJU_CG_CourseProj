8c0d890fc8c4d1236011351e69e2c167805c88bf
ZeamoxWang <wangzimo@zju.edu.cn>
2021-12-14 01:32:28 +0800

    assimp model bingo
    回调函数写的很草率，可能有小bug

--- base/model.h
+++ base/model.h
@@ -9,45 +9,51 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+#include <glad/glad.h> 
 
-#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stb_image.h>
+
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
 using namespace std;
 
 unsigned int TextureFromFile(const char* path, const string& directory, bool gamma = false);
 
-class Model: public Object3D
+class Model
 {
 public:
-	/*  Model Data */
-	vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
-	vector<Mesh> meshes;
-	string directory;
-	bool gammaCorrection;
+    // model data 
+    vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    vector<Mesh>    meshes;
+    string directory;
+    bool gammaCorrection;
 
-	/*  Functions   */
-	// constructor, expects a filepath to a 3D model.
-	Model(string const& path, bool gamma = false) : gammaCorrection(gamma)
-	{
-		loadModel(path);
-	}
+    // constructor, expects a filepath to a 3D model.
+    Model(string const& path, bool gamma = false) : gammaCorrection(gamma)
+    {
+        loadModel(path);
+    }
 
-	// draws the model, and thus all its meshes
-	void Draw(Shader &shader)
-	{
-		for (unsigned int i = 0; i < meshes.size(); i++)
-			meshes[i].Draw(shader);
-	}
+    // draws the model, and thus all its meshes
+    void Draw(Shader& shader);
 
 private:
-	/*  Functions   */
-	// loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
-	void loadModel(string const& path);
+    // loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
+    void loadModel(string const& path);
 
-	// processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
-	void processNode(aiNode* node, const aiScene* scene);
+    // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+    void processNode(aiNode* node, const aiScene* scene);
 
-	Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
 
-	vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
+    // checks all material textures of a given type and loads the textures if they're not loaded yet.
+    // the required info is returned as a Texture struct.
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, string typeName);
 };
+
 
--- base/test.cpp
+++ base/test.cpp
@@ -6,16 +6,66 @@ test::test() {
 	camera.reset(new PerspectiveCamera(glm::radians(45.0f), 1.0f * _windowWidth / _windowHeight, 0.1f, 10000.0f));
 	camera->position.z = 50.0f;
 
+	
+
 	test_shader.reset(new Shader(
-		std::string("D:/2021WinterMaterial/Graphics/CG_proj/shader/test_vertex_shader.txt"),
-		std::string("D:/2021WinterMaterial/Graphics/CG_proj/shader/test_frag_shader.txt")
+		std::string("./shader/test_vertex_shader.txt"),
+		std::string("./shader/test_frag_shader.txt")
 		));
 
-	tmp_model.reset(new Model("D:/2021WinterMaterial/Graphics/CG_proj/data/nanosuit_model/nanosuit.obj"));
+	tmp_model.reset(new Model("./data/nanosuit_model/nanosuit.obj"));
 }
 
 
 void test::handleInput() {
+	const float cameraMoveSpeed = 0.005f;
+	const float cameraRotateSpeed = 0.02f;
+
+	if (_keyboardInput.keyStates[GLFW_KEY_ESCAPE] != GLFW_RELEASE) {
+		glfwSetWindowShouldClose(_window, true);
+		return;
+	}
+
+
+	if (_keyboardInput.keyStates[GLFW_KEY_W] != GLFW_RELEASE) {
+		std::cout << "W" << std::endl;
+		camera->position += cameraMoveSpeed * camera->getFront();
+	}
+
+	if (_keyboardInput.keyStates[GLFW_KEY_A] != GLFW_RELEASE) {
+		std::cout << "A" << std::endl;
+		camera->position -= cameraMoveSpeed * camera->getRight();
+	}
+
+	if (_keyboardInput.keyStates[GLFW_KEY_S] != GLFW_RELEASE) {
+		std::cout << "S" << std::endl;
+		camera->position -= cameraMoveSpeed * camera->getFront();
+	}
+
+	if (_keyboardInput.keyStates[GLFW_KEY_D] != GLFW_RELEASE) {
+		std::cout << "D" << std::endl;
+		camera->position += cameraMoveSpeed * camera->getRight();
+	}
+
+	if (_mouseInput.move.xCurrent != _mouseInput.move.xOld) {
+		std::cout << "mouse move in x direction" << std::endl;
+		const float deltaX = static_cast<float>(_mouseInput.move.xCurrent - _mouseInput.move.xOld);
+		const float angle = -cameraRotateSpeed * _deltaTime * deltaX;
+		const glm::vec3 axis = { 0.0f, 1.0f, 0.0f };
+		glm::quat temp_rotation = { 1.0f * cos(angle), 0.0f, 1.0f * sin(angle), 0.0f };
+		camera->rotation = temp_rotation * camera->rotation;
+		_mouseInput.move.xOld = _mouseInput.move.xCurrent;
+	}
+
+	if (_mouseInput.move.yCurrent != _mouseInput.move.yOld) {
+		std::cout << "mouse move in y direction" << std::endl;
+		const float deltaY = static_cast<float>(_mouseInput.move.yCurrent - _mouseInput.move.yOld);
+		const float angle = -cameraRotateSpeed * _deltaTime * deltaY;
+		const glm::vec3 axis = camera->getRight();
+		glm::quat temp_rotation = { 1.0f * cos(angle), axis[0] * sin(angle), axis[1] * sin(angle), axis[2] * sin(angle) };
+		camera->rotation = temp_rotation * camera->rotation;
+		_mouseInput.move.yOld = _mouseInput.move.yCurrent;
+	}
 	return;
 }
 
@@ -23,7 +73,8 @@ void test::renderFrame() {
 	showFpsInWindowTitle();
 
 	glClearColor(_clearColor.r, _clearColor.g, _clearColor.b, _clearColor.a);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
 
 
 	glm::mat4 projection = camera->getProjectionMatrix();
